dbe1691ff92fe0082b045f36e86b7913753c2774
DESKTOP-3NBEK92\TranVietHieu <hieutran26498@gmail.com>
2024-06-11 17:31:52 +0700

    npro

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,30 @@ import './App.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './Router';
 import { ToastContainer } from 'react-toastify';
+import { useEffect } from 'react';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 function App() {
+  useEffect(() => {
+    const startProgress = () => NProgress.start();
+    const stopProgress = () => NProgress.done();
+
+    // Thiết lập các trình đổi route cho việc bắt đầu và kết thúc NProgress
+    () => startProgress();
+    () => stopProgress();
+
+    // Nghe sự kiện bắt đầu và hoàn thành của việc thay đổi route
+    window.addEventListener('beforeunload', startProgress);
+    window.addEventListener('load', stopProgress);
+
+    return () => {
+      window.removeEventListener('beforeunload', startProgress);
+      window.removeEventListener('load', stopProgress);
+    };
+  }, []);
   return (
     <div>
-      <ToastContainer />
+      <ToastContainer autoClose={3000} />
       <RouterProvider router={router} />
     </div>
   );

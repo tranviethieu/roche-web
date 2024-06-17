535e3e60598289ddba0d3f2457afebb8b393e71a
HIEUTRAN\hieutv.pc <Hieutran26498@gmail.com>
2024-06-17 19:20:16 +0700

    oke

--- src/pages/auth/Login/Login.tsx
+++ src/pages/auth/Login/Login.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+//import { useNavigate } from 'react-router-dom';
 import styles from './Login.module.scss';
 import {
   Button,
@@ -12,74 +12,78 @@ import {
   message,
 } from 'antd';
 import { Link } from 'react-router-dom';
-import { useMutation } from '@tanstack/react-query';
-import axios from 'axios';
+//import { useMutation } from '@tanstack/react-query';
+//import axios from 'axios';
 import { store } from '~/redux/store';
 import { setStateLogin, setToken } from '~/redux/reducer/auth';
-import { delay } from '~/common/func/delay';
+//import { delay } from '~/common/func/delay';
 const { Option } = Select;
 
 const Login: React.FC = () => {
-  const navigate = useNavigate();
+  //const navigate = useNavigate();
   const [spinning, setSpinning] = useState(false);
   const [form] = Form.useForm();
   const onFinish = (values: any) => {
-    login.mutate({ userName: values?.userName, password: values?.password });
+    console.log(values);
+    //login.mutate({ userName: values?.userName, password: values?.password });
+    //test de login
+    setSpinning(true);
+    store.dispatch(setStateLogin(true));
+    store.dispatch(
+      setToken(
+        'eyJhbGciOiJSUzI1NiIsImtpZCI6IjkwQ0JCOTFGRjc4QkNDOTFDMzA4OTJCMThDNDBFRDVDMzlBRkE0MjlSUzI1NiIsInR5cCI6ImF0K2p3dCIsIng1dCI6ImtNdTVIX2VMekpIRENKS3hqRUR0WERtdnBDayJ9.eyJuYmYiOjE3MTg1NDExMjMsImV4cCI6MTcxODYyNzUyMywiaXNzIjoiaHR0cHM6Ly9hcGktbWVhcHAuYmVuaHZpZW4udGVjaC9pZGVudGl0eSIsImNsaWVudF9pZCI6ImFkbWluLmNsaWVudCIsInN1YiI6IjY1ZTU0MjE1ZTUzNWZkZWE3ZTJmYjNjZSIsImF1dGhfdGltZSI6MTcxODU0MTEyMywiaWRwIjoibG9jYWwiLCJlbWFpbCI6ImRlbW9kZEBnbWFpbC5jb20iLCJwaG9uZV9udW1iZXIiOiIwMzg1NzM2MjIyIiwiZ2VuZGVyIjoibWFsZSIsImFkZHJlc3MiOiIiLCJiaXJ0aGRhdGUiOiIwOS8wNy8yMDIzIiwiY2hhdElkIjoiNjVlNTQyMTVlNTM1ZmRlYTdlMmZiM2NmIiwiYXZhdGFyIjoiNjVlNTQyMTVlNTM1ZmRlYTdlMmZiM2NkIiwidXNlck5hbWUiOiJkZW1vZGRAZ21haWwuY29tIiwiZnVsbE5hbWUiOiJUcuG6p24gVmnhu4d0IEhp4bq_dSIsInRpdGxlIjoiIiwid29yayI6IiIsImNvZGUiOiJOSDQzMzMiLCJpc1Jvb3QiOiJGYWxzZSIsImRlcGFydG1lbnRJZCI6IjY1NzEzNDk4YzMyNWU2NmY3MTA0Mjc2MCIsImhvc3BpdGFsSWQiOiI2NTcxMWZlYWMzMjVlNjZmNzEwM2Y4OWUiLCJ2YWx1ZSI6IiIsInJvbGUiOlsiNjVmOTQ5ZjZjYzJlOTM5NjFlNTE3YWMzIiwiNjVlZmNiY2JlYTc5ZWYwYjg4MDM1OTBiIiwiNjVhNjlhMWU2ZDI4YzgzNTRmYTk3ZmU1IiwiNjVhNjlhNDU2ZDI4YzgzNTRmYTk3ZmU2IiwiNjYwMjNiMzNmMTI3NTM1NDA0MzI1NjAwIiwiNjVhNjlhNTc2ZDI4YzgzNTRmYTk3ZmU3IiwiNjVhNjlhODE2ZDI4YzgzNTRmYTk3ZmU5IiwiNjVhNjlhNzE2ZDI4YzgzNTRmYTk3ZmU4IiwiNjVhZjFmZDMzMTc3YzUzMzgzMDM0ZGJmIl0sImp0aSI6IjVDNzRCNTY3RUQwQTdCNENCQjQ0NzMyN0I5QTBDNzhEIiwiaWF0IjoxNzE4NTQxMTIzLCJzY29wZSI6WyJtZWFwcCIsIm9wZW5pZCIsInByb2ZpbGUiLCJvZmZsaW5lX2FjY2VzcyJdLCJhbXIiOlsicHdkIl19.cw7glAVIehiQERTcV3zsZGDIZw5IMMMWIZt2S2NgIkuRMXzvv9wXio8kSiuUMqnhu4y5zkuJveaKjev0f9pD912QSr7b3OxFMnyoDDdxeQp6TNDN738aY8xlpw7XmzSu6Jt9IDosg18HWFIQc9K4Ojl8UGNQTQz4EWpR1yhFfW881m-_8p0B-LggBmStiB_nNqHDqfJzRa8scnFzBX3v3JPnlHn-doFIBomBSqzO8_V30mmSg3XoimGEj_tdz6h60TX8H5IjPDD3FgQCMa5QKivV07S-E7Ns5Kg5a6ICxWmFKh4sNA2J7tnqTb60Ygnez7repdftyobxorKv2lwAjQ'
+      )
+    );
+    message.success('Login success');
+    setSpinning(false);
+    //
   };
   const onCancel = () => {
     form.resetFields();
   };
-  const login = useMutation({
-    mutationFn: async (data: { userName: string; password: string }) => {
-      setSpinning(true);
-      await delay(500);
-      const response = await axios.post(
-        'https://api-meapp.benhvien.tech/identity/connect/token',
-        {
-          client_id: 'admin.client',
-          client_secret: 'meapp-admin-secret',
-          grant_type: 'password',
-          username: data.userName,
-          password: data.password,
-        },
-        {
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            Accept: 'application/json, text/plain, */*',
-          },
-        }
-      );
+  // const login = useMutation({
+  //   mutationFn: async (data: { userName: string; password: string }) => {
+  //     setSpinning(true);
+  //     await delay(500);
+  //     const response = await axios.post(
+  //       'https://api-meapp.benhvien.tech/identity/connect/token',
+  //       {
+  //         client_id: 'admin.client',
+  //         client_secret: 'meapp-admin-secret',
+  //         grant_type: 'password',
+  //         username: data.userName,
+  //         password: data.password,
+  //       },
+  //       {
+  //         headers: {
+  //           'Content-Type': 'application/x-www-form-urlencoded',
+  //           Accept: 'application/json, text/plain, */*',
+  //         },
+  //       }
+  //     );
 
-      return response?.data;
-    },
-    onSuccess(data) {
-      if (data) {
-        setSpinning(false);
-        message.success('Login success');
-        store.dispatch(setStateLogin(true));
-        store.dispatch(setToken(data.access_token));
+  //     return response?.data;
+  //   },
+  //   onSuccess(data) {
+  //     if (data) {
+  //       setSpinning(false);
+  //       message.success('Login success');
+  //       store.dispatch(setStateLogin(true));
+  //       store.dispatch(setToken(data.access_token));
 
-        navigate('/');
-      }
-    },
-    onError(error: any) {
-      setSpinning(false);
-      //test de login
-      store.dispatch(setStateLogin(true));
-      store.dispatch(
-        setToken(
-          'eyJhbGciOiJSUzI1NiIsImtpZCI6IjkwQ0JCOTFGRjc4QkNDOTFDMzA4OTJCMThDNDBFRDVDMzlBRkE0MjlSUzI1NiIsInR5cCI6ImF0K2p3dCIsIng1dCI6ImtNdTVIX2VMekpIRENKS3hqRUR0WERtdnBDayJ9.eyJuYmYiOjE3MTg1NDExMjMsImV4cCI6MTcxODYyNzUyMywiaXNzIjoiaHR0cHM6Ly9hcGktbWVhcHAuYmVuaHZpZW4udGVjaC9pZGVudGl0eSIsImNsaWVudF9pZCI6ImFkbWluLmNsaWVudCIsInN1YiI6IjY1ZTU0MjE1ZTUzNWZkZWE3ZTJmYjNjZSIsImF1dGhfdGltZSI6MTcxODU0MTEyMywiaWRwIjoibG9jYWwiLCJlbWFpbCI6ImRlbW9kZEBnbWFpbC5jb20iLCJwaG9uZV9udW1iZXIiOiIwMzg1NzM2MjIyIiwiZ2VuZGVyIjoibWFsZSIsImFkZHJlc3MiOiIiLCJiaXJ0aGRhdGUiOiIwOS8wNy8yMDIzIiwiY2hhdElkIjoiNjVlNTQyMTVlNTM1ZmRlYTdlMmZiM2NmIiwiYXZhdGFyIjoiNjVlNTQyMTVlNTM1ZmRlYTdlMmZiM2NkIiwidXNlck5hbWUiOiJkZW1vZGRAZ21haWwuY29tIiwiZnVsbE5hbWUiOiJUcuG6p24gVmnhu4d0IEhp4bq_dSIsInRpdGxlIjoiIiwid29yayI6IiIsImNvZGUiOiJOSDQzMzMiLCJpc1Jvb3QiOiJGYWxzZSIsImRlcGFydG1lbnRJZCI6IjY1NzEzNDk4YzMyNWU2NmY3MTA0Mjc2MCIsImhvc3BpdGFsSWQiOiI2NTcxMWZlYWMzMjVlNjZmNzEwM2Y4OWUiLCJ2YWx1ZSI6IiIsInJvbGUiOlsiNjVmOTQ5ZjZjYzJlOTM5NjFlNTE3YWMzIiwiNjVlZmNiY2JlYTc5ZWYwYjg4MDM1OTBiIiwiNjVhNjlhMWU2ZDI4YzgzNTRmYTk3ZmU1IiwiNjVhNjlhNDU2ZDI4YzgzNTRmYTk3ZmU2IiwiNjYwMjNiMzNmMTI3NTM1NDA0MzI1NjAwIiwiNjVhNjlhNTc2ZDI4YzgzNTRmYTk3ZmU3IiwiNjVhNjlhODE2ZDI4YzgzNTRmYTk3ZmU5IiwiNjVhNjlhNzE2ZDI4YzgzNTRmYTk3ZmU4IiwiNjVhZjFmZDMzMTc3YzUzMzgzMDM0ZGJmIl0sImp0aSI6IjVDNzRCNTY3RUQwQTdCNENCQjQ0NzMyN0I5QTBDNzhEIiwiaWF0IjoxNzE4NTQxMTIzLCJzY29wZSI6WyJtZWFwcCIsIm9wZW5pZCIsInByb2ZpbGUiLCJvZmZsaW5lX2FjY2VzcyJdLCJhbXIiOlsicHdkIl19.cw7glAVIehiQERTcV3zsZGDIZw5IMMMWIZt2S2NgIkuRMXzvv9wXio8kSiuUMqnhu4y5zkuJveaKjev0f9pD912QSr7b3OxFMnyoDDdxeQp6TNDN738aY8xlpw7XmzSu6Jt9IDosg18HWFIQc9K4Ojl8UGNQTQz4EWpR1yhFfW881m-_8p0B-LggBmStiB_nNqHDqfJzRa8scnFzBX3v3JPnlHn-doFIBomBSqzO8_V30mmSg3XoimGEj_tdz6h60TX8H5IjPDD3FgQCMa5QKivV07S-E7Ns5Kg5a6ICxWmFKh4sNA2J7tnqTb60Ygnez7repdftyobxorKv2lwAjQ'
-        )
-      );
+  //       navigate('/');
+  //     }
+  //   },
+  //   onError(error: any) {
+  //     setSpinning(false);
 
-      //
-      if (error?.response?.data) {
-        const { data } = error?.response || undefined;
+  //     if (error?.response?.data) {
+  //       const { data } = error?.response || undefined;
 
-        return message.success(data?.error_description || 'Có lỗi xảy ra!');
-      }
-    },
-  });
+  //       return message.success(data?.error_description || 'Có lỗi xảy ra!');
+  //     }
+  //   },
+  // });
 
   return (
     <section className={styles.section}>
